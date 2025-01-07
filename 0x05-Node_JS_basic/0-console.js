6a299701f9f2a9a71aef084a5c1aa08940acb6ef
Moutaz Mahmoud <moutaz.prof.egy@gmail.com>
2025-01-07 23:05:32 +0200

    Feat: console

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Prints a message to the standard output.
+ * @param {string} message - The message to display.
+ */
+function displayMessage(message) {
+    console.log(message);
+  }
+  
+  module.exports = displayMessage;
+  
