2cd20b8d51c952e4f65a4c60d7883f5930262da2
Moutaz Mahmoud <moutaz.prof.egy@gmail.com>
2025-01-07 23:47:27 +0200

    Feat: 0x06-unittests_in_js

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,35 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', () => {
+  describe('SUM', () => {
+    it('should return 6 for ("SUM", 1.4, 4.5)', () => {
+      assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+    });
+  });
+
+  describe('SUBTRACT', () => {
+    it('should return -4 for ("SUBTRACT", 1.4, 4.5)', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    });
+  });
+
+  describe('DIVIDE', () => {
+    it('should return 0.2 for ("DIVIDE", 1.4, 4.5)', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    });
+
+    it('should return "Error" for ("DIVIDE", 1.4, 0)', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    });
+  });
+
+  describe('Invalid type', () => {
+    it('should throw an error for invalid type', () => {
+      assert.throws(() => calculateNumber('MULTIPLY', 1, 2), {
+        name: 'Error',
+        message: 'Invalid operation type',
+      });
+    });
+  });
+});
